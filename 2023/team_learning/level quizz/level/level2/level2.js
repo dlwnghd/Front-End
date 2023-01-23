eaf6cb70389debb960d79862613b4fee5a816343
dlwnghd <alion0513@naver.com>
2023-01-23 23:09:11 +0900

    이주홍#01.23/23:09
    문제 2풀이
    - JS에서 getElementsByName을 사용하여 값을 찾을 때는 배열로 리턴된 값이 오기 때문에 [0]등을 통해 접근하고 value를 사용하야여 한다.
    - addEventListener를 이용하여 click이벤트를 만들때는 preventDefault()를 이용해 페이지가 리렌더링 되는 것을 막아줬다.

--- 2023/team_learning/level quizz/level/level2/level2.js
+++ 2023/team_learning/level quizz/level/level2/level2.js
@@ -1,8 +1,24 @@
 import { RESERVATION_LIST } from "./reservation .js";
-console.log(RESERVATION_LIST);
 
 /* 
 예약 고객확인하기
-
-
 */
+
+var name = document.getElementsByName("user-name");             // 태그의 name이 "user-name"인 Object
+var phone = document.getElementsByName("user-phone");           // 태그의 name이 "user-phone"인 Object
+var reserNum = document.getElementById("reservation-number");   // 태그의 id가 "resrvation-number"인 Object
+var check = true;
+
+// button태그가 클릭 되었을 시
+document.querySelector("button").addEventListener("click", function (event) {
+    event.preventDefault();     // 이벤트의 기본 동작막음(페이지 리랜더링을 막음)
+    RESERVATION_LIST.forEach((item) => {    // ForEach문으로 RESERVATION_LIST의 내의 값을 반복
+        if (item["name"] == name[0].value && item["phone"] == phone[0].value) { // 입력한 값의 이름과 전화번호가 리스트내에 일치하는 정보가 있다면
+            check = false;
+            reserNum.innerText = item["number"];   // reserNum의 내부텍스트에 예약번호를 돌려줌
+        }
+    });
+    if(check){reserNum.innerText = "일치하는 정보가 없습니다. 다시 입력해주세요.";}
+});
+
+
