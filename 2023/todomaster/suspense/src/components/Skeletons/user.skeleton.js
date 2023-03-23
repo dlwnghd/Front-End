67a1fed6063e015c687d7ead0fb330359047cc89
dlwnghd <alion0513@naver.com>
2023-03-23 21:42:21 +0900

    🤢 SickDay : Skeleton에 대한 나름의 정의

--- 2023/todomaster/suspense/src/components/Skeletons/user.skeleton.js
+++ 2023/todomaster/suspense/src/components/Skeletons/user.skeleton.js
@@ -2,6 +2,10 @@ import * as React from 'react';
 import Skeleton from '@mui/material/Skeleton';
 import Stack from '@mui/material/Stack';
 
+/**
+ * 스켈레톤 형태의 UI를 미리 사용자에게 보여줌으로 현재 웹페이지가 멈춘 것이 아닌 데이터를 불러오고 있음을 나타내줌
+ * @returns 
+ */
 const UserSkeleton = () => {
   return (
     <Stack spacing={1}>
